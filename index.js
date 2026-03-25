7808d4b703c0204f17e98d028b3bbffc35cbb853
ohjungwoo08 <ohjungwoo08@gmail.com>
2026-03-26 08:55:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// 메인 페이지
+// 메인 페이지 (public.index.html 연결)
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public.index.html'));
 });
 
-// 페이지 1
+// 페이지 1 (public.page1.html 연결)
 app.get('/page1', (req, res) => {
   res.sendFile(path.join(__dirname, 'public.page1.html'));
 });
 
-// 페이지 2
+// 페이지 2 (public.page2.html 연결)
 app.get('/page2', (req, res) => {
   res.sendFile(path.join(__dirname, 'public.page2.html'));
 });
 
 app.listen(port, () => {
-  console.log('서버가 정상 작동 중입니다!');
+  console.log('서버가 작동 중입니다!');
 });
