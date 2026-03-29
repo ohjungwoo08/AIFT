550b7db39fc7563f24198b38f5659711169fb0a9
ohjungwoo08 <ohjungwoo08@gmail.com>
2026-03-30 08:22:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,16 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// 1. 메인 페이지 접속 시 (https://aift-o2u2.onrender.com/)
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public.index.html'));
 });
 
-// 2. 페이지 1 접속 시 (https://aift-o2u2.onrender.com/page1)
 app.get('/page1', (req, res) => {
   res.sendFile(path.join(__dirname, 'public.page1.html'));
 });
 
-// 3. 페이지 2 접속 시 (https://aift-o2u2.onrender.com/page2)
 app.get('/page2', (req, res) => {
   res.sendFile(path.join(__dirname, 'public.page2.html'));
 });
 
-app.listen(port, () => {
-  console.log(`서버가 포트 ${port}에서 작동 중입니다!`);
-});
+app.listen(port, () => console.log('서버 실행 중!'));
